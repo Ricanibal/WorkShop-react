7cf99d7915f3718e9ddae8acfad05a41c3935e14
Ricanibal <sr.ricanibal8@gmail.com>
2024-02-27 18:40:58 -0500

    Generacion de iconos en la barra lateral

--- src/components/BarraLateral.js
+++ src/components/BarraLateral.js
@@ -1,10 +1,27 @@
 import '../style/BarraLateralStyle.css'
-
+import consola from '../static/icon/consola.png'
+import curriculum from '../static/icon/curriculum.png' 
 export function Barra()
 {
+    let Piezas = [ 
+                    {nombre:'Empleos', icono:curriculum, mensaje:'imgEmpleos'},
+                    {nombre:'Postulaciones', icono:curriculum, mensaje:'imgPostulacion'},
+                    {nombre:'CodeX', icono:consola, mensaje:'imgRetos'},
+                    {nombre:'Preguntas', icono:curriculum, mensaje:'imgPreguntas'},
+                    {nombre:'Bootcamp', icono:curriculum, mensaje:'imgBootcamp'},
+                    {nombre:'Premios', icono:curriculum, mensaje:'imgPremios'},
+                    {nombre:'Tu cv', icono:curriculum, mensaje:'imgCV'},
+                ]
     return(
         <div className='barralaterla'>
-
+            {Piezas.map((user) =>{
+              return (
+                <a className='pieza'>
+                    <img className='imagenbarra' src={user.icono} alt={user.mensaje}/>
+                    <p className='parrafobarra'>{user.nombre}</p>
+                </a>
+              )
+            })}
         </div>
     )
 }
