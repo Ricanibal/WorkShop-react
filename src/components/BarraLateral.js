c2d54060a9c52a8fc74e015c868cc875537a916b
Ricanibal <sr.ricanibal8@gmail.com>
2024-02-28 15:34:48 -0500

    Mejoras en imagenes de los iconos

--- src/components/BarraLateral.js
+++ src/components/BarraLateral.js
@@ -1,15 +1,18 @@
 import '../style/BarraLateralStyle.css'
 import consola from '../static/icon/consola.png'
 import curriculum from '../static/icon/curriculum.png' 
+import empleo from '../static/icon/empleo.png'
+import postulacion from '../static/icon/postulacion.png'
+import cohorchetes from '../static/icon/soportes.png'
+import diamante from '../static/icon/diamante.png'
 export function Barra()
 {
     let Piezas = [ 
-                    {nombre:'Empleos', icono:curriculum, mensaje:'imgEmpleos'},
-                    {nombre:'Postulaciones', icono:curriculum, mensaje:'imgPostulacion'},
+                    {nombre:'Empleos', icono:empleo, mensaje:'imgEmpleos'},
+                    {nombre:'Postulaciones', icono:postulacion, mensaje:'imgPostulacion'},
                     {nombre:'CodeX', icono:consola, mensaje:'imgRetos'},
-                    {nombre:'Preguntas', icono:curriculum, mensaje:'imgPreguntas'},
-                    {nombre:'Bootcamp', icono:curriculum, mensaje:'imgBootcamp'},
-                    {nombre:'Premios', icono:curriculum, mensaje:'imgPremios'},
+                    {nombre:'Bootcamp', icono:cohorchetes, mensaje:'imgBootcamp'},
+                    {nombre:'Premios', icono:diamante, mensaje:'imgPremios'},
                     {nombre:'Tu cv', icono:curriculum, mensaje:'imgCV'},
                 ]
     return(
